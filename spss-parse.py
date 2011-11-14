46079b3b30f6a7387d10cb01b839cb26ee026a71
Philipp <PhilippBay@gmail.com>
2011-11-14 13:50:34 +1000

    Introduced probability-cutoff

--- spss-parse.py
+++ spss-parse.py
@@ -2,9 +2,39 @@
 
 # argparse for command-line options
 import argparse, sys
+import numpy as np
+from neurolab.core import Train, Trainer, TrainStop
+import neurolab as nl
+
+class TrainLM(Train):
+    ''' The training class using Levenberg-Marquardt and the neurolab-package'''
+    def __init__(self, net, input, target, **kwargs):
+        self.net = net
+        self.input = input
+        self.target = target
+        self.kwargs = kwargs
+        self.x = tool.np_get_ref(net)
+        self.full_output = ()
+    
+    def fcn(self, x):
+        self.x[:] = x
+        output = net.sim(self.input)
+        err = self.error(self.net, self.input, self.target, output)
+        try:
+            self.epochf(err, self.net)
+        except TrainStop:
+            pass
+        return (self.target-output).flatten()
+        
+    def __call__(self, net, input, target):
+        from scipy.optimize import leastsq
+        #from scipy.optimize.minpack import error
+        
+        self.full_output = leastsq(func=self.fcn, x0=self.x.copy(), 
+                                    maxfev=self.epochs)
 
 class Measurement:
-    ''' A measurement as predicted by PASW
+    ''' A measurement as predicted by PASW/SPSS
     dis_1 is the predicted group
     dis1_1, dis2_1.. are the probabilities for the group-prediction
     '''
@@ -12,7 +42,7 @@ class Measurement:
     allMeasurements = []
     def __init__(self, dis_1, dis1_1, dis2_1, dis3_1, dis4_1, sample_id, list_of_snps):
         self.predicted_group = dis_1
-        self.list_of_probabilities = [dis1_1,  dis2_1,  dis3_1,  dis4_1] 
+        self.list_of_probabilities = [transformStrToInt(dis1_1),  transformStrToInt(dis2_1), transformStrToInt(dis3_1),  transformStrToInt(dis4_1)] 
         self.sample_id = sample_id
         self.list_of_snps = list_of_snps
         Measurement.allMeasurements.append(self)
@@ -21,23 +51,38 @@ class Measurement:
         return self.predicted_group
 
     def getHighestProbability(self):
-        return max(self.dis1_1, self.dis2_1, self.dis3_1, self.dis4_1)
+        returnstuff = max(self.list_of_probabilities)
+        if returnstuff == " ":
+            return 0
+        else:
+            return float(returnstuff)
 
     def getAllMeasurements():
         return Measurement.allMeasurements
 
+    def getSampleID(self):
+        return self.sample_id
+
     def inspect(self):
         return [self.predicted_group, self.list_of_probabilities, self.sample_id, self.list_of_snps]
 
-def getFileName():
+def transformStrToInt(string):
+    ''' Transforms the way small numbers are presented in SPSS/PASW to the way Python wants them'''
+    if type(string) == str and "E" in string:
+        number = float(string[0:string.index("E")])
+        exponent = int(string[string.index("E")+1:len(string)])
+        return number*(10**exponent)
+    else:
+        return string
+
+def getArguments():
     ''' get the command-line options, return the filename'''
     parser = argparse.ArgumentParser(description ="Parse SPSS/PASW tab-delimited output")
     parser.add_argument("filename", metavar="file", type=str, help="name of the SPSS/PASW output-file")
-    parser.add_argument("--p", metavar="probability", type=int, default = 0, help="Cut-off probability - measurements with probability < prob will not be included")
+    parser.add_argument("-p",  type=float, default = 0, help="Cut-off probability - measurements with probability < prob will not be included")
 
     args = parser.parse_args()
-    toparsename = args.filename
-    return toparsename
+    return args
 
 def tryToOpenFile(toparsename):
     ''' try to open the file and return the object, print IOError else'''
@@ -57,7 +102,7 @@ def getFormatOfOutput(line):
     line = line.replace("\n","").split("\t")
     fielddict = {}
     fielddict["startOfSNPList"] = 1
-    fielddict["endOfSNPList"] = line.index("Skincolour")
+    fielddict["endOfSNPList"] = line.index("Skincolour") # THIS IS GOING TO BE A PROBLEM
     fielddict["dis_1"] = line.index("Dis_1")
     fielddict["dis1_1"] = line.index("Dis1_1")
     fielddict["dis2_1"] = line.index("Dis2_1")
@@ -67,34 +112,47 @@ def getFormatOfOutput(line):
             fielddict["dis5_1"] = line.index("Dis5_1")
     return fielddict
 
-def parse_file(fields, linelist, list_of_measurements):
+def parse_file(fields, linelist, cutoff):
     ''' Go through the entire file, create measurement-objects '''
     linecounter = 1
+    list_of_measurements = []
     # look at all lines
     while linecounter < len(linelist):
             currentLine = linelist[linecounter].replace("\n", "").split("\t")
             sample_id = currentLine[0]
             if len(currentLine) != 1: # some lines only contain a \n
                     m = Measurement(currentLine[fields["dis_1"]], currentLine[fields["dis1_1"]], currentLine[fields["dis2_1"]],currentLine[fields["dis3_1"]], currentLine[fields["dis4_1"]], sample_id, currentLine[1:fields["endOfSNPList"]], )
-                    list_of_measurements.append(m)
+                    if m.getHighestProbability() < cutoff:
+                        print >> sys.stderr, "Measurement %s is not going to be included" %m.getSampleID()
+                    else:
+                        list_of_measurements.append(m)
             linecounter += 1
     return list_of_measurements
 
 def clean_list(list_of_measurements):
-    for element in list_of_measurements:
-        print(element.inspect())
+    ''' go through the list of measurements, kick out elements with missing stuff '''
+    # going over a copy of the list because else counters get confused by removing elements
+    for m in list(list_of_measurements):
+        if m.getProbabilityGroup() == " ":
+            print >> sys.stderr, "Sample-ID %s is incomplete, removing" %m.getSampleID()
+            list_of_measurements.remove(m)
+        
     return list_of_measurements
 
 def main():
+    ''' The main-method - which calls all other methods'''
     # get the filename
-    toparsename = getFileName()
+    args = getArguments()
+    toparsename = args.filename
     # get all lines of the file in one big list
     # should be replaced by "for line in file", .readlines() is quite memory-hungry
     linelist = tryToOpenFile(toparsename).readlines()
     fields = getFormatOfOutput(linelist[0])
-    list_of_measurements = []
-    parse_file(fields, linelist, list_of_measurements)
+    list_of_measurements = parse_file(fields, linelist, args.p)
     list_of_measurements = clean_list(list_of_measurements)
+
+    # now go and create 1 ANN for each Measurement
+    for m in list_of_measurements:
 
 
 main()
